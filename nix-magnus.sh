959ff077084e49e15a095c402ceb60ab2938a4cf
fs <fabrice@fabricesemti.com>
2024-05-05 12:49:23 +0100

    fix: 🚑️ add `sudo`

--- nix-magnus.sh
+++ nix-magnus.sh
@@ -9,9 +9,9 @@ export CURRENT_HOST=$(hostname)
 # Check if the current host matches the TARGET_HOST
 if [ "$CURRENT_HOST" = "$TARGET_HOST" ]; then
   # Perform local build
-  nixos-rebuild switch --flake .#${TARGET_HOST}
+  sudo nixos-rebuild switch --flake .#${TARGET_HOST}
 else
   # Perform remote build
-  nixos-rebuild switch --flake .#${TARGET_HOST} \
+  sudo nixos-rebuild switch --flake .#${TARGET_HOST} \
     --target-host root@${TARGET_HOST} --build-host root@${TARGET_HOST} --verbose
 fi
