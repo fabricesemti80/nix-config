99567348b4de61df3ebedfab882eeadd84a539ed
fs <fabrice@fabricesemti.com>
2024-05-05 12:48:29 +0100

    fix: 🚑️ ommit `--build-host`

--- nix-magnus.sh
+++ nix-magnus.sh
@@ -3,14 +3,13 @@
 #TODO: replace this with just/make/tasks!
 
 # Get current hostname
-TARGET_HOST=magnus
-CURRENT_HOST=$(hostname)
+export TARGET_HOST=magnus
+export CURRENT_HOST=$(hostname)
 
 # Check if the current host matches the TARGET_HOST
 if [ "$CURRENT_HOST" = "$TARGET_HOST" ]; then
   # Perform local build
-  nixos-rebuild switch --flake .#${TARGET_HOST} \
-    --build-host localhost --verbose
+  nixos-rebuild switch --flake .#${TARGET_HOST}
 else
   # Perform remote build
   nixos-rebuild switch --flake .#${TARGET_HOST} \
