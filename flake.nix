a5eeb500b6e7cdc6db19ad8b06335ee839b52f99
fs <fabrice@fabricesemti.com>
2024-05-05 13:28:19 +0100

    feat: ⚡️ updat inputs
    reference: https://github.com/EmergentMind/nix-config/blob/dev/flake.nix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,54 @@
 {
-  description = "Your new nix config";
+  description = "Fabrice Semti's Nix config";
 
   inputs = {
-    # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    # You can access packages and modules from different nixpkgs revs
-    # at the same time. Here's an working example:
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
+    
+    #################### ? -------------------------------------------------------------------> Official NixOS and HM Package Sources 
+
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-23.11";
+    };
+
+    nixpkgs-unstable.\ = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    hardware = {
+      url = "github:nixos/nixos-hardware";
+    };
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+        url = "github:nix-community/home-manager/release-23.11";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    # VSCode
-    vscode-server.url = "github:nix-community/nixos-vscode-server";
+    #################### ? ------------------------------------------------------------------->  Utilities 
+
+    # Declarative partitioning and formatting
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Secrets management. See ./docs/secretsmgmt.md
+    sops-nix = {
+      url = "github:mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    vscode-server.url =
+      "github:nix-community/nixos-vscode-server"; # VSCode Server support - "https://github.com/nix-community/nixos-vscode-server"
+
+
+    #################### ? -------------------------------------------------------------------> Personal Repositories 
+
+    # Private secrets repo.  See ./docs/secretsmgmt.md
+    # Authenticate via ssh and use shallow clone
+    nix-secrets = {
+      url = "git+ssh://git@gitlab.com/fabricesemti/nix-secrets.git?ref=main&shallow=1";
+      flake = false;
+    };
+
   };
 
   outputs = {
