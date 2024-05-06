e8bf24a594e15535862ba7a8ba77fa78e91854a9
fs <fabrice@fabricesemti.com>
2024-05-06 13:14:33 +0100

    feat: ⚡️ add Git aliases
    Ref: https://github.com/fabricesemti80/nix-starter/blob/dev/modules/home-manager/git.nix

--- home/fs/common/core/git.nix
+++ home/fs/common/core/git.nix
@@ -1,11 +1,41 @@
 { pkgs, lib, config, ... }:
+
+let
+  gitAliases = {
+    l = ''
+      log --pretty=format:"%C(yellow)%h\ %ad%Cred%d\ %Creset%s%Cblue\ [ %cn ]" --decorate --date=short'';
+    a = "add";
+    ap = "add -p";
+    c = "commit --verbose";
+    ca = "commit -a --verbose";
+    cm = "commit -m";
+    cam = "commit -a -m";
+    m = "commit --amend --verbose";
+    d = "diff";
+    ds = "diff --stat";
+    dc = "diff --cached";
+    s = "status -s";
+    co = "checkout";
+    cob = "checkout -b";
+    # # list branches sorted by last modified
+    b =
+      "!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'";
+    # # list aliases
+    la = "!git config -l | grep alias | cut -c 7-";
+    # # https://davidwalsh.name/awesome-git-aliases
+    dad = "!curl https://icanhazdadjoke.com/ && echo";
+    last = "log - 1 HEAD";
+  };
+
+in
 {
+
   programs.git = {
     enable = true;
     package = pkgs.gitAndTools.gitFull;
     userName = "emergentmind";
     userEmail = "2889621-emergentmind@users.noreply.gitlab.com";
-    aliases = { };
+    aliases = gitAliases;
     extraConfig = {
       init.defaultBranch = "main";
       url = {
@@ -18,7 +48,6 @@
       };
 
       user.signing.key = "41B7B2ECE0FAEF890343124CE8AA1A8F75B56D39";
-      #TODO sops - Re-enable once sops setup complete
       commit.gpgSign = false;
       gpg.program = "${config.programs.gpg.package}/bin/gpg2";
     };
