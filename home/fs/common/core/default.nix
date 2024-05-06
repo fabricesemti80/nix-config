e0d0b70f614de846f24beb5f307490905eb438c9
fs <fabrice@fabricesemti.com>
2024-05-06 12:28:57 +0100

    Fixed ZSH
    Finally fixed ZSH [no need for 'rec']

--- home/fs/common/core/default.nix
+++ home/fs/common/core/default.nix
@@ -14,7 +14,7 @@
     ./screen.nix # hopefully rarely needed but good to have if so
     ./ssh.nix # personal ssh configs
     ./zoxide.nix # cd replacement
-    # ./zsh # primary shell: includes zsh, oh-my-zsh, and p10k theme #FIXME: fix pkgs and re-enable
+    ./zsh # primary shell: includes zsh, oh-my-zsh, and p10k theme 
 
     # TODO: Not set, need to investigate but will need custom config if used:
     # ./shellcolor.nix
