58bb3fa118f7d2320a2f973f94952ddd9be92b5d
fs <fabrice@fabricesemti.com>
2024-05-07 07:20:24 +0100

    add deadnix suggestions

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,11 +1,7 @@
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
-{
-  inputs,
-  outputs,
-  lib,
-  config,
-  pkgs,
+{ outputs
+, config
   ...
 }: {
   # You can import other home-manager modules here
@@ -44,15 +40,6 @@
       allowUnfree = true;
     };
   };
-
-  # home = {
-  #   username = "fs";
-  #   homeDirectory = "/home/fs";
-  # };
-
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
