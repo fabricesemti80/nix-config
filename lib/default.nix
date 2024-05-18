3946a48b4a5fe04a5ec2d5e9accc1b00fc8a5fd9
fs <fabrice@fabricesemti.com>
2024-05-18 22:31:14 +0100

    updates - misc

--- lib/default.nix
+++ lib/default.nix
@@ -1,5 +1,15 @@
-{ lib, ... }:
-{
+{ lib, ... }: {
+  colmenaSystem = import ./colmenaSystem.nix;
+  macosSystem = import ./macosSystem.nix;
+  nixosSystem = import ./nixosSystem.nix;
+
+  attrs = import ./attrs.nix { inherit lib; };
+
+  genK3sServerModule = import ./genK3sServerModule.nix;
+  genK3sAgentModule = import ./genK3sAgentModule.nix;
+  genKubeVirtHostModule = import ./genKubeVirtHostModule.nix;
+  genKubeVirtGuestModule = import ./genKubeVirtGuestModule.nix;
+
   # use path relative to the root of the project
   relativeToRoot = lib.path.append ../.;
   scanPaths = path:
