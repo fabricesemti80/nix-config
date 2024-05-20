3dc090e68bad08554baf4f439cfdfe97982beca1
fabrice <fs@fabricesemti.com>
2024-05-20 19:33:56 +0100

    test ready

--- flake.nix
+++ flake.nix
@@ -6,10 +6,10 @@
       # ---- SYSTEM SETTINGS ---- #
       systemSettings = {
         system = "x86_64-linux"; # system arch
-        hostname = "snowfire"; # hostname
-        profile = "personal"; # select a profile defined from my profiles directory
-        timezone = "America/Chicago"; # select timezone
-        locale = "en_US.UTF-8"; # select locale
+        hostname = "horus"; # hostname
+        profile = "homelab"; # select a profile defined from my profiles directory
+        timezone = "Europe/London"; # select timezone
+        locale = "en_GB.UTF-8"; # select locale
         bootMode = "uefi"; # uefi or bios
         bootMountPath = "/boot"; # mount path for efi boot partition; only used for uefi boot mode
         grubDevice = ""; # device identifier for grub; only used for legacy (bios) boot mode
@@ -17,10 +17,10 @@
 
       # ----- USER SETTINGS ----- #
       userSettings = rec {
-        username = "emmet"; # username
-        name = "Emmet"; # name/identifier
-        email = "emmet@librephoenix.com"; # email (used for certain configurations)
-        dotfilesDir = "~/.dotfiles"; # absolute path of the local repo
+        username = "fs"; # username
+        name = "Fabrice Semti"; # name/identifier
+        email = "fabrice@fabricesemti.com"; # email (used for certain configurations)
+        dotfilesDir = "~/projects/nix-config"; # absolute path of the local repo
         theme = "uwunicorn-yt"; # selcted theme from my themes directory (./themes/)
         wm = "hyprland"; # Selected window manager or desktop environment; must select one in both ./user/wm/ and ./system/wm/
         # window manager type (hyprland or x11) translator
--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -5,84 +5,34 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [ (modulesPath + "/profiles/qemu-guest.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ zenpower ];
-  hardware.opengl.extraPackages = [ pkgs.amdvlk ];
-  hardware.opengl.extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
-
-  systemd.tmpfiles.rules = [
-    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
-  ];
-
-  # my stupid usb hub crashes systemct suspend half of the time now
-  # https://wiki.archlinux.org/title/Power_management/Suspend_and_hibernate#Sleep_hooks
-  systemd.services.root-suspend = {
-    enable = true;
-    description = "Root systemd suspend prehook";
-    unitConfig = {
-      Description = "Root systemd suspend prehook";
-      Before = "sleep.target";
-    };
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.uhubctl}/bin/uhubctl -a off";
-    };
-    wantedBy = [ "sleep.target" ];
-  };
-  systemd.services.root-resume = {
-    enable = true;
-    description = "Root systemd suspend posthook";
-    unitConfig = {
-      Description = "Root systemd suspend posthook";
-      After = "suspend.target";
-    };
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.uhubctl}/bin/uhubctl -a on";
-    };
-    wantedBy = [ "suspend.target" ];
-  };
-
-  boot.kernel.sysctl = {
-    "vm.swappiness" = 90;
-    "vm.vfs_cache_pressure" = 50;
-    "vm.dirty_background_ratio" = 2;
-    "vm.dirty_ratio" = 5;
-  };
-
-  services.btrfs.autoScrub = {
-    enable = true;
-    interval = "weekly";
-  };
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/637d8261-0650-4ece-a35b-59d97baf64a7";
-      fsType = "btrfs";
-      options = [ "noatime,compress-force=zstd:2,discard=async,commit=120,clear_cache,space_cache=v2,subvol=@" ];
+    { device = "/dev/disk/by-uuid/8189534f-8a8b-4a63-bb19-d88dca5d3331";
+      fsType = "ext4";
     };
 
-  boot.initrd.luks.devices."luks-385106b5-71f7-460e-9a2b-2416f3b54cb6".device = "/dev/disk/by-uuid/385106b5-71f7-460e-9a2b-2416f3b54cb6";
-
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/F09D-73C9";
+    { device = "/dev/disk/by-uuid/BC7C-2A7F";
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/6b1eea4e-b7ac-4281-9da8-4ce29c076fe1"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp5s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.ens18.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
